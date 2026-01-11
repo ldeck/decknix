856139b6b30486e7c2b7977f0dc22f4cb48a8d9c
Lachlan Deck <lachlan.deck@gmail.com>
2026-01-12 10:36:51 +1100

    feat: pin to nix 25.11; fix flake template location

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "decknix: Shared Team Nix Configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
@@ -10,12 +10,12 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    nix-parts.url = "github:hercules-ci/nix-parts";
+    nix-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ self, nix-parts, ... }:
-    nix-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" ];
+  outputs = inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "aarch64-darwin" "x86_64-darwin" "aarch64-linux" "x86_64-linux" ];
 
       perSystem = { config, self', inputs', pkgs, system, ... }: {
         # Devshell for working on decknix itself
@@ -39,21 +39,21 @@
         homeModules = {
           default = ./modules/home/default.nix;
         };
-      };
 
-      templates = {
-        default = {
-          path = ./templates/default;
-          description = "Standard Decknix User Configuration";
-          welcomeText = ''
-            # Welcome to Decknix!
-
-            To finish setup:
-            1. Edit 'flake.nix' and update 'username' and 'hostname'.
-            2. Run 'nix run nix-darwin -- switch --flake .#default --impure'
-
-            Enjoy!
-          '';
+        templates = {
+          default = {
+            path = ./templates/default;
+            description = "Standard Decknix User Configuration";
+            welcomeText = ''
+              # Welcome to Decknix!
+  
+              To finish setup:
+              1. Edit 'flake.nix' and update 'username' and 'hostname'.
+              2. Run 'nix run nix-darwin -- switch --flake .#default --impure'
+  
+              Enjoy!
+            '';
+          };
         };
       };
     };
