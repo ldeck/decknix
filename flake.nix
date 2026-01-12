279f3b2752c54e0304b382bb97d6edf5e24b7706
Lachlan Deck <lachlan.deck@gmail.com>
2026-01-12 12:57:16 +1100

    fix: pin inputs correctly to nixpkgs 25.11

--- flake.nix
+++ flake.nix
@@ -4,10 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
-    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-25.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nix-parts.url = "github:hercules-ci/flake-parts";
