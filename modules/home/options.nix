c23d82392244caf086bba706c7527edf532e0066
Lachlan Deck <lachlan.deck@gmail.com>
2026-01-14 17:55:47 +1100

    fix: disable post activation check
    its checks are too simplistic.

--- modules/home/options.nix
+++ modules/home/options.nix
@@ -4,7 +4,7 @@ with lib;
 
 let
   cfg = config.decknix;
-  
+
   # Define your templates here as multi-line strings
   templates = {
     developer = ''
@@ -24,7 +24,7 @@ let
         ];
 
         #home.sessionVariables = {
-        #  EDITOR = "nano"; # override default 'vim' 
+        #  EDITOR = "nano"; # override default 'vim'
         #};
 
         # This overrides the upstream Starship symbol
@@ -55,29 +55,29 @@ in {
   config = {
     # We use Home Manager's activation system
     # This runs AFTER the build, but BEFORE the generation is marked 'current'
-    home.activation.ensureLocalConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      LOCAL_CONFIG="$HOME/.local/decknix/config.nix"
-      
-      if [ ! -f "$LOCAL_CONFIG" ]; then
-        echo -e "\033[0;31m[Decknix] Local configuration missing at $LOCAL_CONFIG\033[0m"
-        echo -e "\033[0;34m[Decknix] Generating template for role: ${cfg.role}...\033[0m"
-        
-        mkdir -p "$(dirname "$LOCAL_CONFIG")"
-        
-        # Inject the template content based on the role selected in flake.nix
-        cat > "$LOCAL_CONFIG" <<EOF
-      ${if cfg.role == "developer" then templates.developer
-        else if cfg.role == "designer" then templates.designer
-        else "{ ... }: { }"}
-      EOF
+    # home.activation.ensureLocalConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    #   LOCAL_CONFIG="$HOME/.local/decknix/default/home.nix"
 
-        echo -e "\033[0;32m[Decknix] Template created successfully.\033[0m"
-        echo -e "\033[1;33m[ACTION REQUIRED] Please edit $LOCAL_CONFIG to fill in your details.\033[0m"
-        echo -e "\033[1;31mAborting activation to prevent applying incomplete config.\033[0m"
-        
-        # This aborts the switch!
-        exit 1
-      fi
-    '';
+    #   if [ ! -f "$LOCAL_CONFIG" ]; then
+    #     echo -e "\033[0;31m[Decknix] Local configuration missing at $LOCAL_CONFIG\033[0m"
+    #     echo -e "\033[0;34m[Decknix] Generating template for role: ${cfg.role}...\033[0m"
+
+    #     mkdir -p "$(dirname "$LOCAL_CONFIG")"
+
+    #     # Inject the template content based on the role selected in flake.nix
+    #     cat > "$LOCAL_CONFIG" <<EOF
+    #   ${if cfg.role == "developer" then templates.developer
+    #     else if cfg.role == "designer" then templates.designer
+    #     else "{ ... }: { }"}
+    #   EOF
+
+    #     echo -e "\033[0;32m[Decknix] Template created successfully.\033[0m"
+    #     echo -e "\033[1;33m[ACTION REQUIRED] Please edit $LOCAL_CONFIG to fill in your details.\033[0m"
+    #     echo -e "\033[1;31mAborting activation to prevent applying incomplete config.\033[0m"
+
+    #     # This aborts the switch!
+    #     exit 1
+    #   fi
+    # '';
   };
 }
